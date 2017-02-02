7b7c0403b9c7ce30ba7d0d0d6913100e5d92eb8c
Miguel Anderson <mra21@duke.edu>
2017-02-02 12:37:03 -0500

    Removed redundant classes

--- src/cellsociety_team13/Location.java
+++
@@ -1,9 +0,0 @@
-package cellsociety_team13;
-
-import java.awt.Point;
-
-public class Location extends Point{
-	public Location(double x, double y){
-		
-	}
-}
--- src/cellsociety_team13/Poly.java
+++
@@ -1,5 +0,0 @@
-package cellsociety_team13;
-
-public abstract class Poly {
-
-}
