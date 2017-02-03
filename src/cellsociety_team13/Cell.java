c6bfcc99c97a394c1416c79dfbd6507477313ebc
Matthew Tribby <mrt28@duke.edu>
2017-02-03 02:28:04 -0500

    Fixing organization error

---
+++ src/cellsociety_team13/Cell.java
@@ -0,0 +1,14 @@
+package cellsociety_team13;
+import javafx.scene.paint.Color;
+
+public abstract class Cell {
+		private Color state;
+
+		protected Cell(Color colorInput){
+			this.state = colorInput;
+		}
+
+		Color getState(){
+			return this.state;
+		}		
+}
